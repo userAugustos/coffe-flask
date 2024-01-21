108fc34b3bed49ef30f50ac423d1eb607e8ce02d
Felipe Augustos <felipe_augustos84@outlook.com.br>
2024-01-21 12:14:28 -0300

    feat: added upddate locations

--- main.py
+++ main.py
@@ -24,24 +24,67 @@ class Users(Resource):
     
     print(args)
     
-    new_data = pd.DataFrame({
-      'userId': args['userId'],
-      'name': args['name'],
-      'city': args['city'],
-      'locations': [[]]
-    })
-    
-    #read CSV
+    # read CSV
     data = pd.read_csv('users.csv')
     
-    #app the provided values
+    if args['userId'] in list(data['userId']):
+      return {
+        'message': f"{args['userId']} already exists."
+      }
+      
+    new_data = pd.DataFrame({
+    'userId': args['userId'],
+    'name': args['name'],
+    'city': args['city'],
+    'locations': [[]]
+  })
+    
+    # app the provided values
     data = data._append(new_data, ignore_index=True)
     
-    #save the CSV
+    # save the CSV
     
     data.to_csv('users.csv', index=False)
     
     return { 'data': data.to_dict() }, 200
+  
+  def put(self):
+    parser = reqparse.RequestParser()
+    
+    parser.add_argument('userId', required=True)
+    parser.add_argument('location', required=True)
+    
+    args = parser.parse_args()
+    
+    data = pd.read_csv('users.csv')
+    
+    print(list(data['userId']))
+    
+    # so we search if the passed id existis in the csv
+    if args['userId'] in list(data['userId']):
+      # if it does, we transform the location csv in mutiple lists
+      data['locations'] = data['locations'].apply(
+        lambda x: ast.literal_eval(x)
+      )
+      print(type(data['locations']))
+      
+      # search in our csv if user passed already has that location 
+      for index, item in enumerate(data['userId']):
+        if item == args['userId']:
+          print(f"find user {item}, in {index}")
+          for item in data['locations'][index]:
+            if item == args['location']:
+              return { 'message': f"Location {args['location']} already exists in {data['locations'][index]}" }, 404
+      
+      user_data = data[data['userId'] == args['userId']]
+      
+      user_data['locations'] = user_data['locations'].values[0].append(args['location'])
+      
+      data.to_csv('users.csv', index=False)
+      
+      return { 'data': data.to_dict() }, 200  
+    
+    return { 'message': f"{args['userId']} user not found" }, 404
 
 class Locations(Resource):
   pass
